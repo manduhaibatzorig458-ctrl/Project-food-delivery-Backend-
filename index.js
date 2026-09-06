51a357024b3dbbe580ca7bf656068cccc22d8ee0
user <manduhai.batzorig458@gmail.com>
2026-09-06 21:02:42 +0800

    Update project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import { User } from "./schemas/user-schema.js";
 import { connectDB } from "./connectDB.js";
+import { FoodCategory } from "./schemas/food-category-schema.js";
 
 const app = express();
 const PORT = 1000;
@@ -37,7 +38,45 @@ app.post("/login", async (request, response) => {
       response.status(500).json({message: "Internal Server Error" ,error: error.message });
   }
 })
+
 // FOOD CAREGORY 
+
+ app.get("/food-category", async (request, response) => {
+  try {
+    const foodCategories = await FoodCategory.find();
+    response.status(200).json({ message: "food categories fetched", foodCategories });
+  } catch (error) {
+    response.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
+})
+
+app.post("/food-category", async (request, response) => {
+  try {
+    const { categoryName } = request.body;
+    const foodCategory = await FoodCategory.create({ categoryName });
+ 
+    console.log("--> Шинэ ангилал амжилттай хадгалагдлаа:", foodCategory);
+ 
+    response.status(201).json({ message: "food category created", foodCategory });
+  } catch (error) {
+    console.log("error", error);
+    response.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
+})
+
+app.delete("/food-category/:foodCategoryId", async (request, response) => {
+  try {
+    const { foodCategoryId } = request.params;
+    const foodCategory = await FoodCategory.findByIdAndDelete(foodCategoryId);
+    if (!foodCategory) {
+      return response.status(404).json({ message: "Food category not found" });
+    }
+    response.status(200).json({ message: "food category deleted", foodCategory });
+  } catch (error) {
+    response.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
+})
+
 
 
 app.listen(PORT, () => {
