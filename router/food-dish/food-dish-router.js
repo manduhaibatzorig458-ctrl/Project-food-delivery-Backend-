26e67fc39319329133d0ed8894c4dd40201eb9a7
user <manduhai.batzorig458@gmail.com>
2026-09-23 12:25:21 +0800

    Update project

--- router/food-dish/food-dish-router.js
+++ router/food-dish/food-dish-router.js
@@ -1,23 +1,3 @@
-// import express from "express";
-
-// import { getFoodDishController } from "../../controllers/food-dish/get-food-dish.js";
-// import { deleteFoodDishController } from "../../controllers/food-dish/delete-food-dish.js";
-// import { updateFoodDishController } from "../../controllers/food-dish/update-food-dish.js";
-// import { getFoodDishByCategoryController } from "../../controllers/food-dish/get-fooddish-by-category.js";
-// import { createFoodDishController } from "../../controllers/food-dish/create-food-dish.js";
-
-// const foodDishRouter = express.Router();
-
-// foodDishRouter.get("/food/get", getFoodDishController);
-// foodDishRouter.get("/food/get/:categoryId", getFoodDishByCategoryController);
-// foodDishRouter.post("/create", createFoodDishController);
-// foodDishRouter.put("/food/:foodId", updateFoodDishController);
-// foodDishRouter.delete("/food/:foodId", deleteFoodDishController);
-
-// export default foodDishRouter;
-
-
-
 import express from "express";
 
 import { getFoodDishController } from "../../controllers/food-dish/get-food-dish.js";
